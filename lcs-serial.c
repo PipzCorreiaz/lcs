f014972ce9993963b5c03223162c4755391f540e
Filipa Correia <filipaincorreia@gmail.com>
2014-10-26 22:32:28 +0000

    Add reverse search to build the common subsequence.

--- lcs-serial.c
+++ lcs-serial.c
@@ -28,36 +28,36 @@ int letter_index(char alpha[4], char letter) {
 }
 
 short cost(int x) {
-	int i, n_iter = 20;
-	double dcost = 0;
-	for (i = 0; i < n_iter; i++) {
-		dcost += pow(sin((double) x), 2) + pow(cos((double) x), 2);
-	}
+    int i, n_iter = 20;
+    double dcost = 0;
+    for (i = 0; i < n_iter; i++) {
+        dcost += pow(sin((double) x), 2) + pow(cos((double) x), 2);
+    }
 
-	return (short) (dcost / n_iter + 0.1);
+return (short) (dcost / n_iter + 0.1);
 }
 
 
 int main(int argc, char const *argv[]) {
     FILE *file;
 
-	int seq1_size = 0;
-	char *seq1, *seq2;
-	int seq2_size = 0;
+    int seq1_size = 0;
+    char *seq1, *seq2;
+    int seq2_size = 0;
 
     file = fopen(argv[1], "r");
-	if (file != NULL) {
+    if (file != NULL) {
         fscanf(file, "%d %d", &seq1_size, &seq2_size);
 
-		seq1 = (char *) calloc(seq1_size + 1, sizeof(char));
-		seq2 = (char *) calloc(seq2_size + 1, sizeof(char));
+        seq1 = (char *) calloc(seq1_size + 1, sizeof(char));
+        seq2 = (char *) calloc(seq2_size + 1, sizeof(char));
 
         fscanf(file, "%s %s", seq1, seq2);
 
-	} else {
-		printf("%s\n", "Unable to open file");
-		exit(-1);
-	}
+    } else {
+        printf("%s\n", "Unable to open file");
+        exit(-1);
+    }
 
     fclose(file);
 
@@ -66,10 +66,10 @@ int main(int argc, char const *argv[]) {
 
     P = (int *) calloc(ALPHA_SIZE * (seq2_size + 1), sizeof(int));
 
-	int *matrix = (int*) calloc((seq1_size + 1) * (seq2_size + 1), sizeof(int));
+    int *matrix = (int*) calloc((seq1_size + 1) * (seq2_size + 1), sizeof(int));
     if (matrix == NULL) {
-    	printf("Not enough memory. Learning how to program in C might help...\n");
-    	exit(-1);
+        printf("Not enough memory. Learning how to program in C might help...\n");
+        exit(-1);
     }
 
     int i, j;
@@ -78,33 +78,26 @@ int main(int argc, char const *argv[]) {
 
     /* Compute P table */
     for (i = 0; i < ALPHA_SIZE; i++) {
-    	int index = i * gap;
-    	char c = C[i];
-    	for (j = 0; j < seq2_size + 1; j++) {
-    		char b = (j - 2) < 0 ? '#' : seq2[j - 2];
-    		/*printf("(%c, %c) ", c, b);*/
+        int index = i * gap;
+        char c = C[i];
+        for (j = 0; j < seq2_size + 1; j++) {
+            char b = (j - 2) < 0 ? '#' : seq2[j - 2];
 
-    		if (j == 0) {
-    			P[index + j] = j;
-    		} else if (b == c) {
-    			P[index + j] = j - 1;
-    		} else {
-				P[index + j] = P[index + j - 1];
-    		}
-
-    		/*printf("%d ", P[index + j]);*/
-
-    	}
-
-    	/*printf("\n");*/
-
+            if (j == 0) {
+                P[index + j] = j;
+            } else if (b == c) {
+                P[index + j] = j - 1;
+            } else {
+                P[index + j] = P[index + j - 1];
+            }
+        }
     }
 
-	for (i = 1; i < seq1_size + 1; i++) {
-		int index = i * gap;
-		for (j = 1; j < seq2_size + 1; j++) {
-			char xi = seq1[i - 1];
-			/*char yj = seq2[j - 1];*/
+
+    for (i = 1; i < seq1_size + 1; i++) {
+        int index = i * gap;
+        for (j = 1; j < seq2_size + 1; j++) {
+            char xi = seq1[i - 1];
             int l_value = letter_index(C, xi);
             int p_value = P[l_value * gap + j];
 
@@ -120,40 +113,55 @@ int main(int argc, char const *argv[]) {
                     matrix[index + j] = other_cell;
                 }
             }
-
-		}
-	}
-
-/*    print_new_matrix(matrix, seq1_size, seq2_size);
-
-	i = seq1_size;
-	j = seq2_size;
-	char xi;
-	int last_cell = matrix[i * gap + j];
-	int len = last_cell;
-	char lcs[len + 1];
-	lcs[len] = '\0';
-
-	while(last_cell > 0) {
-		int index = i * gap + j;
-		xi = seq1[i - 1];
-        int next_cell = matrix[index - gap];
-		if (next_cell < last_cell) {
-            lcs[last_cell - 1] = xi;
         }
-
-        last_cell = next_cell;
-        i--;
-        j--;
-	}
-
-	printf("%d\n%s\n", len, lcs);
-*/
-    printf("%d\n", matrix[seq1_size * gap + seq2_size]);
-	free(matrix);
-	free(seq1);
-	free(seq2);
+    }
 
 
-	return 0;
+    i = seq1_size;
+    j = seq2_size;
+    char xi, yj;
+    int last_cell = matrix[i * gap + j];
+    int len = last_cell;
+    char lcs[len + 1];
+    lcs[len] = '\0';
+
+    while(i > 0 && j > 0) {
+        int index = i * gap + j;
+        xi = seq1[i - 1];
+        yj = seq2[j - 1];
+
+        if (xi == yj) {
+            last_cell--;
+            lcs[last_cell] = xi;
+            i--;
+            j--;
+        } else {
+            int main = matrix[index];
+            int top = matrix[index - gap];
+            int left = matrix[index - 1];
+            int right_of_top = matrix[index - gap + 1];
+
+            if (right_of_top > top) {
+                top++;
+            } 
+            if (main > left) {
+                left++;
+            } 
+
+            if (left >= top) {
+                j--;
+            } else {
+                i--;
+            }
+        }
+    }
+
+    printf("%d\n%s\n", len, lcs);
+
+    free(matrix);
+    free(seq1);
+    free(seq2);
+
+
+    return 0;
 }
